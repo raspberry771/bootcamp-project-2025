6044f3bbcff8d62a77acc47fc760fccf59a75739
raspberry771 <raspberry11771@gmail.com>
2025-12-11 00:19:17 -0800

    added promise to fix npm run build

--- src/app/api/blog/[slug]/route.ts
+++ src/app/api/blog/[slug]/route.ts
@@ -2,20 +2,21 @@ import { NextRequest, NextResponse } from 'next/server'
 import connectDB from "../../../../database/db";
 import Blog from "../../../../database/blogSchema";
 
+// 1. UPDATE: params is now a Promise
 type IParams = {
-    params: {
+    params: Promise<{
         slug: string
-    }
+    }>
 }
 
 export async function GET(req: NextRequest, { params }: IParams) {
-    await connectDB() // 1. Connect to the database
-    const { slug } = params // 2. Grab the "slug" from the URL
+    await connectDB() 
+    
+    // 2. UPDATE: You must 'await' params before you can access 'slug'
+    const { slug } = await params 
 
     try {
-        // 3. Query: Find ONE blog where the slug matches
         const blog = await Blog.findOne({ slug }).orFail()
-        // 4. Return: Send that blog back as JSON
         return NextResponse.json(blog)
     } catch (err) {
         return NextResponse.json('Blog not found.', { status: 404 })
